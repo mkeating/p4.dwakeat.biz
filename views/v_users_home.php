e5af283cb5502f9ec36be05b3364a577465bbe98
Michael Keating <mkeating2225@gmail.com>
2013-12-09 16:24:06 -0500

    slight change to home

--- views/v_users_home.php
+++ views/v_users_home.php
@@ -3,17 +3,24 @@
 <br>
 <!-- if no current Tale 
  form for New Tale -->
+ <?php if($user->current_tale == NULL): ?>
  New Tale
-<form method='POST' action='' role='form'>
+<form method='POST' action='/tales/p_new' role='form'>
 	<div class='form_group'>
 		<input type='text' name='title' class="form-control" placeholder='Title'>
 	</div>
 	<br>
 	<div class='form_group'>
-		<label for='password'>Password</label>
-		<input type='text' name='email' class="form-control" placeholder='Email of who to pass along to'>
+		<label for='content'>Start a story:</label>
+		<input type='text' name='content' class="form-control">
+	</div>
+	<br>
+	<div class='form_group'>
+		<label for='email_next'>Pass it along</label>
+		<input type='text' name='email_next' class="form-control" placeholder='Email of who to pass along to'>
 	</div>
 	<br>
 	<button type='submit' class='btn btn-default'>Submit</button>
 </form>
+<?php endif ?>
 <!--else, display current Tale -->
