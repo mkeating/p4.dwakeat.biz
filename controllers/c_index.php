23943e2095a11d3c4249eb240856446fe1943542
Michael Keating <mkeating2225@gmail.com>
2013-12-12 16:51:11 -0500

    preliminary add client files

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -27,13 +27,13 @@ class index_controller extends base_controller {
 			$this->template->title = "HumbleTales";
 	
 		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
+			
+			$client_files_head = Array("/css/custom.css", "/js/respond.js");
+	    	$this->template->client_files_head = Utils::load_client_files($client_files_head);
 	    	
-	    	$client_files_body = Array("");
+	    	$client_files_body = Array("http://code.jquery.com/jquery-latest.min.js", "/js/bootstrap.min.js");
 	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
+	    	
 	      					     		
 		# Render the view
 			echo $this->template;
